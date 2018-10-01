f080a4508206ebf7886379e9f5203a5444f00fc4
Carlos Müller <kdu428@gmail.com>
2018-09-30 21:23:08 -0300

    [wip] comandline command executor

---
+++ src/main/java/com/mullercarlos/cli/CommandExecuter.java
@@ -0,0 +1,31 @@
+package com.mullercarlos.cli;
+
+
+import lombok.RequiredArgsConstructor;
+
+import java.io.*;
+
+@RequiredArgsConstructor
+public class CommandExecuter {
+    private final String command;
+
+    public String execute() throws IOException {
+        ProcessBuilder builder = new ProcessBuilder();
+        builder.command("sh", "-c", command);
+        builder.directory(new File(System.getProperty("user.home")));
+        Process process = builder.start();
+        BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+        StringBuilder response = new StringBuilder();
+        do{
+            line = input.readLine();
+            response.append(line).append('\n');
+        }while(input.ready());
+        assert process.exitValue() == 0;
+        return response.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.print(new CommandExecuter("ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%mem | head").execute());
+    }
+}
