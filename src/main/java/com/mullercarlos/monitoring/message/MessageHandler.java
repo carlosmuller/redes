ef00d1e2c642b2f2d3f55258caee41a47397b76f
Carlos Müller <kdu428@gmail.com>
2018-10-09 22:08:14 -0300

    [wip]Começo da mensagem de Follow

--- src/main/java/com/mullercarlos/monitoring/message/MessageHandler.java
+++ src/main/java/com/mullercarlos/monitoring/message/MessageHandler.java
@@ -37,77 +37,79 @@ public class MessageHandler extends Thread {
          * Quando clients é null significa que quem está recendo mensagem é o cliente os tipos são {@link com.mullercarlos.monitoring.message.Type#START START}, {@link com.mullercarlos.monitoring.message.Type#STOP STOP}
          * e {@link com.mullercarlos.monitoring.message.Type#FOLLOW FOLLOW}
          */
-        if(clients != null){//Mensagens do cliente para o Servidor
-            if(message instanceof Signin){
+        if (clients != null) {//Mensagens do cliente para o Servidor
+            if (message instanceof Signin) {
                 Signin signin = (Signin) message;
                 ClientModel clientModel = ClientModel.builder().authKey(signin.getAuthKey()).serviceList(signin.getServiceList()).port(signin.getPortListener())
                         .ip(socket.getInetAddress()
                                 .getHostAddress()).build();
                 String authKey = clientModel.getAuthKey();
-                if(clients.containsKey(authKey)){
+                if (clients.containsKey(authKey)) {
                     if (verbose) {
-                        System.out.println(UUID + " - Existe um cliente com a chave: ["+ authKey+"]");
+                        System.out.println(UUID + " - Existe um cliente com a chave: [" + authKey + "]");
                     }
-                    ClientModel existent = (ClientModel)clients.get(authKey);
-                    if(!existent.equals(clientModel)){//bloqueia caso usem a a mesma chave mas são diferentes
+                    ClientModel existent = (ClientModel) clients.get(authKey);
+                    if (!existent.equals(clientModel)) {//bloqueia caso usem a a mesma chave mas são diferentes
                         if (verbose) {
-                            System.out.println(UUID + " - BLOQUEADO - Os clientes eram diferentes então bloqueie existent["+existent+"] da mensagem["+clientModel+"]");
+                            System.out.println(UUID + " - BLOQUEADO - Os clientes eram diferentes então bloqueie existent[" + existent + "] da mensagem[" + clientModel + "]");
                         }
                         sendMessage(new Failed("not allowed"));
                         return;
                     }
                 }
                 if (verbose) {
-                    System.out.println(UUID + " - Colocando o cliente no mapa de autorizados: ["+clientModel+"]");
+                    System.out.println(UUID + " - Colocando o cliente no mapa de autorizados: [" + clientModel + "]");
                 }
                 clients.putIfAbsent(authKey, clientModel);
                 sendMessage(new Ok("Consegui te cadstrar com sucesso", authKey));
                 return;
             }
-            if (message instanceof Health){
+            if (message instanceof Health) {
                 Health healthUpdate = ((Health) message);
                 String authKey = healthUpdate.getAuthKey();
-                if(clients.containsKey(authKey)){//Para tratar essa mensagem o cliente tem que ter mandado antes a de SINGIN
-                    ClientModel signedIn = (ClientModel)clients.get(authKey);
+                if (clients.containsKey(authKey)) {//Para tratar essa mensagem o cliente tem que ter mandado antes a de SINGIN
+                    ClientModel signedIn = (ClientModel) clients.get(authKey);
                     String hostAddress = socket.getInetAddress().getHostAddress();
-                    if(!signedIn.getIp().equals(hostAddress)){// uma chave só pode responder para um ip
+                    if (!signedIn.getIp().equals(hostAddress)) {// uma chave só pode responder para um ip
                         if (verbose) {
-                            System.out.println(UUID + " - BLOQUEADO - Os ips diferem para a chave["+ authKey +"] cadastrado["+signedIn.getIp()+"] da mensagem["+hostAddress+"]");
+                            System.out.println(UUID + " - BLOQUEADO - Os ips diferem para a chave[" + authKey + "] cadastrado[" + signedIn.getIp() + "] da mensagem[" + hostAddress + "]");
                         }
                         sendMessage(new Failed("not allowed"));
                         return;
                     }
                     signedIn.updateHealth(healthUpdate);
                     sendMessage(new Ok("Health updated", authKey));
-                }else{//Bloqueia caso não tenha ainda sido cadastrado
+                } else {//Bloqueia caso não tenha ainda sido cadastrado
                     if (verbose) {
-                        System.out.println(UUID + " - BLOQUEADO - Por não achar o a chame no mapa chave["+authKey+"]");
+                        System.out.println(UUID + " - BLOQUEADO - Por não achar o a chame no mapa chave[" + authKey + "]");
                     }
                     sendMessage(new Failed("You should send signin first!"));
                     return;
                 }
             }
-        }else{//Mensagens do servidor para o cliente
-            if(message instanceof Follow){
-                Follow follow = (Follow)message;
-                if(follow.getAuthKey().equals("")){
+        } else {//Mensagens do servidor para o cliente
+            if (message instanceof Follow) {
+                Follow follow = (Follow) message;
+                if (follow.getAuthKey().equals("")) {
                     String pathOfFile = follow.getPathOfFile();
                     Path path = Paths.get(pathOfFile);
-                    if(!Files.isReadable(path)){
+                    if (!Files.isReadable(path)) {
                         if (verbose) {
-                            System.out.println(UUID + " - FALHO - Porque o arquivo não é legivel["+pathOfFile+"]");
+                            System.out.println(UUID + " - FALHO - Porque o arquivo não é legivel[" + pathOfFile + "]");
                         }
                         sendMessage(new Failed("Can't read"));
                         return;
                     }
                     try {
                         BufferedReader bufferedReader = Files.newBufferedReader(path);
-                        while (true){
+                        while (true) {//atualiza o server a cada 100 milisegundos
                             String s = bufferedReader.readLine();
-                            if(s ==null){
+                            if (s == null) {
                                 sleep(100);
                             }
                             this.output.println(s);
+                            if (socket.isClosed()) return;
+                            else sleep(100);
                         }
                     } catch (IOException e) {
                         e.printStackTrace();
@@ -121,7 +123,7 @@ public class MessageHandler extends Thread {
 
     public void sendMessage(Message message) {
         String messageJson = JSONUtils.serialize(message);
-        if(this.verbose) System.out.println(UUID + " - Enviando a mensagem : ["+message+"]");
+        if (this.verbose) System.out.println(UUID + " - Enviando a mensagem : [" + message + "]");
         this.output.println(messageJson);
     }
 
@@ -137,7 +139,7 @@ public class MessageHandler extends Thread {
         //Por limitação da lib tive que converter duas vezes uma para a classe Mãe Message e outra para o a classe que está no Type
         Message message = JSONUtils.deserialize(jsonString, Message.class);
         message = (Message) JSONUtils.deserialize(jsonString, message.getType().getClazz());
-        if(this.verbose) System.out.println(UUID + " - Recebi a mensagem : ["+message+"]");
+        if (this.verbose) System.out.println(UUID + " - Recebi a mensagem : [" + message + "]");
         return message;
     }
 
